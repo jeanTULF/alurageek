7a662e427e3d98fb388ec82bd516495ac2af1c40
Jean <jeanpolo15@gmail.com>
2023-11-21 06:13:36 -0500

    validaciones

--- scripts/validaciones/validarFormulario.js
+++ scripts/validaciones/validarFormulario.js
@@ -1,12 +1,14 @@
 let nombre = document.getElementById("nombre");
 let formulario = document.getElementById("formulario");
 const regex = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+const inputs = document.querySelectorAll('#formulario input');
 
 formulario.addEventListener("submit", (e) => {
     if(!regex.test(nombre.value)){
+        alert(`el nombre ${nombre.value} no es valido`)
+        
         e.preventDefault()
-        alert("el nombre no es valido", nombre)
     } else {
         alert("se ha enviado el formulario correctamente")
     }
-})
+});
