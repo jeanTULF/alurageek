ca4011a0ca189f5dfd1288b980b0a480363d3195
Jean <jeanpolo15@gmail.com>
2024-03-14 00:31:28 -0500

    Funcionalidad de la searchbar (filtrado)

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -1,11 +1,49 @@
+import { productsService } from "../service/products-service.js";
+
 const searchbar = document.querySelector('.header__item-searchbar-input');
 
 const resultBox = document.querySelector('.result-box');
 
-searchbar.addEventListener('focus', (e) => {
+searchbar.addEventListener('keyup', (e) => {
     resultBox.style.display = "flex";
-})
+    let input = searchbar.value.toLowerCase();
 
-searchbar.addEventListener('blur', (e) => {
-    resultBox.style.display = "none";
-})
+    if(input.length){
+        productsService.listaProductos().then((data) => {
+            let resultado = data.filter((producto) => {
+                return producto.nombre.toLowerCase().includes(input);
+            });
+            console.log(resultado);
+
+            // Borra los resultados anteriores
+            resultBox.innerHTML = '';
+
+            // Selecciona el <ul> después de borrar los resultados anteriores
+            const ul = document.createElement('ul');
+
+            resultado.slice(0, 6).forEach((producto) => {
+                const resultadoElemento = document.createElement('li')
+                const contenido = `
+                    <p>${producto.nombre}</p>
+                    <img src=${producto.urlImagen} alt=${producto.nombre}>
+                `
+                resultadoElemento.innerHTML = contenido;
+
+                resultadoElemento.addEventListener('click', () => {
+
+                    // Construye la URL con el ID del producto y redirige a la página producto.html
+                    const url = `/screens/producto.html?id=${producto.id}`;
+                    window.location.href = url;
+                });
+                ul.appendChild(resultadoElemento);
+            });
+
+            // Agrega el <ul> con los nuevos resultados al resultBox
+            resultBox.appendChild(ul);
+        });
+    } else {
+        resultBox.innerHTML = '';
+    }
+});
+
+
